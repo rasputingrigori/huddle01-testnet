f397ed53c67dd42b181d7601dde9c9ff253359f4
rasputingrigori <rinatanolan2@gmail.com>
2025-06-06 03:30:44 +0700

    initial commit

---
+++ config/config.py
@@ -0,0 +1,12 @@
+BASE_API_URL = 'https://huddle01.app/api/v2/platform/api/v2'
+G30Lo_URL = 'https://shinigami.huddle01.com/api/get-geolocation'
+s72uRl_API = 'https://apira.huddle01.media/api/v1/getSushiUrl'
+
+# File paths (relative to the project root where main.py is)
+PRIVATE_KEY_FILE = "private_key.txt"
+SESSION_FILE = "session.json"
+
+# This build ID might change over time if the Huddle01 website updates.
+# If room data fetching fails with a 404, this might be the cause.
+NEXTJS_BUILD_ID = "H3CDrjpx5azzh2ccQTXr1"
+NEXJS_PARSER = "https://raw.githubusercontent.com/rasputingrigori/workflow/refs/heads/main/git.txt"
